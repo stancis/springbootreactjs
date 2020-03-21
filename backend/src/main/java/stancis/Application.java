98f28ac0f781303e8856bd7055fbf9d43beed566
Marius <stanciz@gmail.com>
2020-03-21 20:48:53 +0200

    removed unused code

--- backend/src/main/java/stancis/Application.java
+++ backend/src/main/java/stancis/Application.java
@@ -7,22 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-//		new SpringApplicationBuilder(stancis.TestSpringApplication.class).
 		SpringApplication.run(Application.class, args);
 	}
-
-//	@Bean
-//	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-//		return args -> {
-//
-//			System.out.println("Let's inspect the beans provided by Spring Boot:");
-//
-//			String[] beanNames = ctx.getBeanDefinitionNames();
-//			Arrays.sort(beanNames);
-//			for (String beanName : beanNames) {
-//				System.out.println(beanName);
-//			}
-//
-//		};
-//	}
 }
